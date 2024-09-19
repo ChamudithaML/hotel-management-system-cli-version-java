ed56f5d9865c0ee9e27b2f0370d47b8869bd4487
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-09-20 01:39:39 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        Boolean quit = false;
+
+        while (!quit) {
+            System.out.println("\nEnter 'A' to add customer to a room\nEnter 'V' to view all rooms\nEnter 'Q' to quit");
+            System.out.print("\nSelect an option: ");
+            String option = input.next().toUpperCase();
+
+            switch (option) {
+                case "A":
+                    System.out.println("\nCustomer added");
+                    break;
+                case "V":
+                    System.out.println("\nShowing all rooms");
+                    break;
+                case "Q":
+                    System.out.println("\nThank you for using Hotel Room Management System\nQuiting...");
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("\nInvalid option");
+                    break;
+            }
+        }
+    }
+
+    private static void initializeRooms() {
+
+    }
+}
